30d0c5dc1fa7f539e939c211abbd21549f00a128
Sourcery AI <>
2022-06-23 10:24:05 +0000

    'Refactored by Sourcery'

--- Part 1- Introduction to Computer Vision/1_1_Image_Representation/helpers.py
+++ Part 1- Introduction to Computer Vision/1_1_Image_Representation/helpers.py
@@ -12,18 +12,18 @@ import cv2
 # The list contains all images and their associated labels
 # For example, after data is loaded, im_list[0][:] will be the first image-label pair in the list
 def load_dataset(image_dir):
-    
+
     # Populate this empty image list
     im_list = []
     image_types = ["day", "night"]
 
     # Iterate through each color folder
     for im_type in image_types:
-        
+
         # Iterate through each image file in each image_type folder
         # glob reads in any image with the extension "image_dir/im_type/*"
         for file in glob.glob(os.path.join(image_dir, im_type, "*")):
-            
+
             # Read in the image
             im = mpimg.imread(file)
 
